35549ccb067bf4067f3fc7e84062b47104ed6215
Vargath <jrtvgz@gmail.com>
2025-03-18 17:24:41 +0100

    merge 1

---
+++ cartuchada/3-Validators/ValidationConstants.cs
@@ -0,0 +1,11 @@
+
+namespace _3_Validators
+{
+    internal struct ValidationConstants
+    {
+        public const int _PRODUCT_TYPE_GAME_BOY_CARTDRIGE = 1;
+        public const int _PRODUCT_TYPE_GAME_GEAR_CARTDRIGE = 2;
+        public const int _PRODUCT_TYPE_GAME_BOY_CONSOLE = 3;
+        public const int _PRODUCT_TYPE_GAME_GEAR_CONSOLE = 4;
+    }
+}
